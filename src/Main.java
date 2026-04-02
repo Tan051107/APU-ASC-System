3b2eafbe13d3c1f0dc9fa37d599882ac63c9a5cf
Sylvester Ng <Sylvesterng123@gmail.com>
2026-04-02 15:15:24 +0800

    Updated main.java

--- src/Main.java
+++ src/Main.java
@@ -1,12 +1,9 @@
 import ui.pages.Login;
-import ui.pages.managermenu;
 
 import javax.swing.*;
 
 void main() {
-    /* SwingUtilities.invokeLater(()->new Login().createUI()); */
-    SwingUtilities.invokeLater(() -> {
-            new managermenu().setVisible(true);
-    });
+    SwingUtilities.invokeLater(()->new Login().createUI());
+    
 }
 
